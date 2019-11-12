f34b6cb2e472dbddb38de9e92d628ffb404df820
Shaanukstar123 <55187271+Shaanukstar123@users.noreply.github.com>
2019-11-12 23:36:00 +0000

    Update Main Game.py

--- Main Game.py
+++ Main Game.py
@@ -3,20 +3,44 @@ import pygame
 from Network import Network
 from player import Player
 from MainMenu import menu
+from player import Map
 
 def main_game():
-    width = 900
-    height = 550
+    width = 1300
+    height = 700
+    backgrounds=[]
+
+    anime1=pygame.image.load("sprites/Background anime/frame0.gif")
+    anime2=pygame.image.load("sprites/Background anime/frame1.gif")
+    anime3=pygame.image.load("sprites/Background anime/frame2.gif")
+    anime4=pygame.image.load("sprites/Background anime/frame3.gif")
+    anime5=pygame.image.load("sprites/Background anime/frame4.gif")
+    anime6=pygame.image.load("sprites/Background anime/frame5.gif")
+    anime7=pygame.image.load("sprites/Background anime/frame6.gif")
+    anime8=pygame.image.load("sprites/Background anime/frame7.gif")
+
+    backgrounds.append(pygame.transform.scale(anime1,(width,height)))
+    backgrounds.append(pygame.transform.scale(anime2,(width,height)))
+    backgrounds.append(pygame.transform.scale(anime3,(width,height)))
+    backgrounds.append(pygame.transform.scale(anime4,(width,height)))
+    backgrounds.append(pygame.transform.scale(anime5,(width,height)))
+    backgrounds.append(pygame.transform.scale(anime6,(width,height)))
+    backgrounds.append(pygame.transform.scale(anime7,(width,height)))
+    backgrounds.append(pygame.transform.scale(anime8,(width,height)))
+
+
+
+
     gameDisplay = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Client")
-    background=pygame.image.load('forest_background.jpg')
-    gameover=pygame.image.load("gameover.jpg")
-    gameover=pygame.transform.scale(background,(width,height))
+    #background=pygame.image.load('animatedbackground.gif')
+    #gameover=pygame.image.load("gameover.jpg")
+    #gameover=pygame.transform.scale(background,(width,height))
     #background=pygame.transform.scale(background,(width, height))
-
-    def redraw_window(gameDisplay,player, player2):
+    def redraw_window(gameDisplay,player, player2,background_index):
+        background_index=int(background_index)
         gameDisplay.fill((255,255,255))
-        gameDisplay.blit(background,(0,0))
+        gameDisplay.blit(backgrounds[background_index],(0,0))
 
         playerObj = player['player']
         secondPlayerObj = player2['player']
@@ -65,8 +89,12 @@ def main_game():
         playerObj = p['player']
 
         clock = pygame.time.Clock()
-
+        background_index=-1
         while run:
+            if background_index>6:
+                background_index=0
+            else:
+                background_index+=0.15
             clock.tick(60)
             p2 = n.send(p)
             secondPlayerObj = p2['player']
@@ -77,7 +105,7 @@ def main_game():
 
             playerObj.move()
             p['player'] = playerObj
-            redraw_window(gameDisplay,p, p2)
+            redraw_window(gameDisplay,p, p2,background_index)
             pygame.display.update()
 
     main()
