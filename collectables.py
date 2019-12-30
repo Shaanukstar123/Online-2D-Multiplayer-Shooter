d410b9cb6673f55a3a9fe0fed3d6398623df2318
Shaanukstar123 <55187271+Shaanukstar123@users.noreply.github.com>
2019-12-30 16:57:14 +0000

    Added new Class: Collectable_list and levitate method onto Collectables class
    Instead of having a list variable, I've made it into an class object.
    After a lot of testing, I found out that the server kept overwriting the collectable item data, so any change from the client side could not be processed. Next step is to move most of the processing to the client side and leave only the instantiation on the server side.

--- collectables.py
+++ collectables.py
@@ -8,44 +8,60 @@ class Collectable_list:
         self.items = []
 
     def add(self, item):
-        pass
+        self.items.append(item)
 
     def get(self, id):
         pass
+    def remove(self, item):
+        self.items.remove(item)
 
 class Collectable():
     def __init__(self):
-        self.item=None
+        self.type=random.randint(1,3) # health, super-speed and invulnerability
         self.sprite=None
         self.x=random.randint(0,width-50)
         self.y=random.randint(0,height-50)
         self.type=None
         self.life = random.randint(300,2000)
-        self.hitbox=pygame.Rect(self.x,self.y,20,20)
+        self.hitbox=pygame.Rect(self.x,self.y,25,25)
         self.correct_position=False
         self.status=None
+        self.collected=False
+        self.counter=0
+        self.vanish=False
 
     def generate(self,walls):
-        self.x=random.randint(0,width)
-        self.y=random.randint(0,height)
-        self.hitbox=pygame.Rect(self.x,self.y,40,40)
+        self.x=random.randint(0,width-50)
+        self.y=random.randint(0,height-50)
+        self.hitbox=pygame.Rect(self.x,self.y,25,25)
+
         for wall in walls:
             if self.hitbox.colliderect(wall):
                 self.correct_position=False
+                return False
+                #self.generate(walls)
             else:
                 self.correct_position=True
-        if self.correct_position==False:
-            self.generate(wall)
+                return True
 
-    def collected(self,player):
-        if self.colliderect(player.hitbox,self.hitbox):
-            return True
+    def levitate(self):
+        self.counter+=1
+        if self.counter<40:
+            self.y+=0.2
+        else:
+            self.y-=0.2
+            if self.counter>80:
+                self.counter=0
+
+    def collect(self,player):
+        pass
+        '''if self.hitbox.colliderect(player.hitbox):
+            self.collected=True
+            print("Item Collected")
+            return True'''
 
 
     def display(self,gameDisplay,images,index):
         for image in images:
             gameDisplay.blit(images[index],(self.x,self.y))
-        object = pygame.Rect(self.x,self.y,40,40)
-        #pygame.draw.rect(gameDisplay,(99,99,99), object)
-
-    #def spawn(self):
+            #pygame.draw.rect(gameDisplay,(99,99,99), self.hitbox)
